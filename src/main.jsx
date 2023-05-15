2a61b7c12610cb4ae7007ba745fd1a3f8cdb2fc0
UttejK <uttej09092001@gmail.com>
2023-05-15 21:12:08 +0530

    019

--- src/main.jsx
+++ src/main.jsx
@@ -2,19 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.scss";
 
-import {
-  createBrowserRouter,
-  Outlet,
-  RouterProvider,
-  useParams,
-} from "react-router-dom";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import ErrorPage from "./pages/ErrorPage/ErrorPage";
 
 import Home from "./pages/Home/Home";
 import Explore from "./pages/Explore/Explore";
 import About from "./pages/About/About";
-import Contact from "./pages/Contact/Contact";
 
 const router = createBrowserRouter([
   {
--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: { port: 3000 },
-  base: "/AdvancedDrone",
+  base: "/AdvancedDrone/",
 });
